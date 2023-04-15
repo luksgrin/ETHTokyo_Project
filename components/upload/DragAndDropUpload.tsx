427733cce738af6638e30f053e78380af9c92f13
Parallels <parallels@debian-gnu-linux-11.localdomain>
2023-04-15 05:15:40 -0400

    adds base64 conversion

--- components/upload/DragAndDropUpload.tsx
+++ components/upload/DragAndDropUpload.tsx
@@ -5,9 +5,23 @@ const fileTypes = ["MP3"];
 
 function DragDrop() {
   const [file, setFile] = useState(null);
+
+  const getBase64 = (file: any, cb: (result: any) => any) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        cb(reader.result)
+    };
+    reader.onerror = function (error) {
+        console.log('Error: ', error);
+    };
+  }
+
   const handleChange = (file: React.SetStateAction<null>) => {
     setFile(file);
-    console.log({file})
+    getBase64(file, (base64encoded) => {
+        console.log({file, base64encoded});
+    })
   };
   return (
     <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -2,7 +2,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import DragAndDropUpload from '../components/DragAndDropUpload';
+import DragAndDropUpload from '../components/upload/DragAndDropUpload';
 
 const Home: NextPage = () => {
   return (
