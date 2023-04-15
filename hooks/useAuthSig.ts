d6113ab670629e1a0f28f982ce45f3bc73b46e49
Parallels <parallels@debian-gnu-linux-11.localdomain>
2023-04-15 05:52:24 -0400

    fixes infinite render

--- hooks/useAuthSig.ts
+++ hooks/useAuthSig.ts
@@ -1,7 +1,9 @@
 import * as LitJsSdk from "@lit-protocol/lit-node-client";
+import { useState } from "react";
 
 export const useAuthSig = () => {
-    const authSig = LitJsSdk.checkAndSignAuthMessage({ chain: "ethereum" }).then((msg) => console.log(msg));
-
+    const [sig, setSig] = useState<any>(null);
+    if (!sig) LitJsSdk.checkAndSignAuthMessage({ chain: "ethereum" }).then((msg) => setSig(msg));
+    return sig;
 }   
 
--- hooks/useLitClient.ts
+++ hooks/useLitClient.ts
@@ -1,13 +1,17 @@
 import * as LitJsSdk from "@lit-protocol/lit-node-client";
 import { useState } from "react";
+import { useProvider } from "wagmi";
 
 const clients: { [chain: string]: any} = {};
 
 export const useLitClient = ({chain}: { chain: string}) => {
+    const provider = useProvider({chainId: chain == 'ethereum' ? 1 : 1});
+    const [connected, setConnected] = useState(false);
     const [_client, setClient] = useState(null);
     if (clients[chain]) return clients[chain];
 
     const client = new LitJsSdk.LitNodeClient({});
+    clients[chain] = client;
     client.connect().then((client) => setClient(client));
 
     return _client;
